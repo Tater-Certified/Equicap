0bfcd3e895f2f3e89cf843868c3da2589273272c
d-catte <qpcrummer@gmail.com>
2025-06-24 14:07:45 -0400

    Improve command results

--- src/main/java/com/github/tatercertified/equicap/MobCapCommand.java
+++ src/main/java/com/github/tatercertified/equicap/MobCapCommand.java
@@ -8,54 +8,25 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.DimensionArgumentType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class MobCapCommand {
     public static void registerCommand() {
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, environment) ->
                 dispatcher.register(CommandManager.literal("equicap")
-                        .executes(context -> {
-                            if (context.getSource().isExecutedByPlayer()) {
-                                ServerPlayerEntity player = context.getSource().getPlayer();
-                                MutableText text = Text.empty();
-                                boolean first = true;
-                                for (SpawnGroup group : SpawnGroup.values()) {
-                                    if (group == SpawnGroup.MISC) {
-                                        continue;
-                                    }
-                                    int total = ((MobCapTracker)player).adjustedMobCapMaxSize(group);
-                                    int current = ((MobCapTracker)player).getPlayerMobCap(group);
-                                    Formatting color;
-                                    float percent = (float) current / total;
-                                    if (percent >= 1.0) {
-                                        color = Formatting.RED;
-                                    } else if (percent > 0.75) {
-                                        color = Formatting.YELLOW;
-                                    } else {
-                                        color = Formatting.GREEN;
-                                    }
-                                    if (!first) {
-                                        text.append("\n");
-                                    } else {
-                                        first = false;
-                                    }
-                                    text.append(Text.literal(group.asString() + ": ")).append(Text.literal(current + "/" + total).formatted(color));
-                                }
-                                context.getSource().sendFeedback(() -> text, false);
-                                return 1;
-                            } else {
-                                context.getSource().sendError(Text.of("This command must be executed by a player"));
-                                return 0;
-                            }
-                        })
                         .then(CommandManager.literal("set")
                                 .then(CommandManager.argument("group", StringArgumentType.string())
                                         .suggests(SPAWN_GROUP_SUGGESTIONS)
@@ -99,6 +70,71 @@ public class MobCapCommand {
                                         })
                                 )
                         )
+                        .then(CommandManager.literal("debug")
+                                .then(CommandManager.literal("player")
+                                        .executes(context -> {
+                                            MutableText text = getPrintOut(null, context);
+                                            if (text != null) {
+                                                context.getSource().sendFeedback(() -> text, false);
+                                                return 1;
+                                            } else {
+                                                return 0;
+                                            }
+                                        })
+                                )
+                                .then(CommandManager.literal("dimension")
+                                        .executes(context -> {
+                                            MutableText text = getPrintOut(MobCapTracker.getDimensionMobCount(context.getSource().getWorld()), context);
+                                            if (text != null) {
+                                                context.getSource().sendFeedback(() -> text, false);
+                                                return 1;
+                                            } else {
+                                                return 0;
+                                            }
+                                        })
+                                        .then(CommandManager.argument("dimension", DimensionArgumentType.dimension())
+                                                .executes(context -> {
+                                                    ServerWorld world = DimensionArgumentType.getDimensionArgument(context, "dimension");
+                                                    MutableText text = getPrintOut(MobCapTracker.getDimensionMobCount(world), context);
+                                                    if (text != null) {
+                                                        context.getSource().sendFeedback(() -> text, false);
+                                                        return 1;
+                                                    } else {
+                                                        return 0;
+                                                    }
+                                                })
+                                        )
+                                )
+                                .then(CommandManager.literal("global")
+                                        .executes(context -> {
+                                            MutableText text = getPrintOut(MobCapTracker.getTotalMobCount(context.getSource().getServer()), context);
+                                            if (text != null) {
+                                                context.getSource().sendFeedback(() -> text, false);
+                                                return 1;
+                                            } else {
+                                                return 0;
+                                            }
+                                        })
+                                )
+                        )
+                        .then(CommandManager.literal("help")
+                                .executes(context -> {
+                                    context.getSource().sendFeedback(() -> Text.of(
+                                            """
+                                                    Welcome to EquiCap, a per-player mob cap solution
+                                                    Command Usage:
+                                                    - debug: Prints out the number of mobs in the specified scope (player, dimension, or global)
+                                                    - get: Gets the max player mob cap size
+                                                    - merge: Sets the mob cap merging mode:
+                                                        None: No merging when players are close
+                                                        Combine: Divide the player's mob cap size by the number of near players
+                                                        Vanillalike: Decrease the player's mob cap size based on the proximity of the nearest player
+                                                    - set: Sets the max player mob cap size
+                                                    """
+                                    ), false);
+                                    return 1;
+                                })
+                        )
                 )
         );
     }
@@ -131,5 +167,50 @@ public class MobCapCommand {
                 .filter(g -> g.getName().equals(input))
                 .findFirst()
                 .orElseThrow(() -> CommandSyntaxException.BUILT_IN_EXCEPTIONS.literalIncorrect().create("Invalid merge mode: " + input));
+    }
+
+    @Nullable
+    private static MutableText getPrintOut(@Nullable EnumMap<SpawnGroup, int[]> data, CommandContext<ServerCommandSource> context) {
+        if (context.getSource().isExecutedByPlayer()) {
+            ServerPlayerEntity player = context.getSource().getPlayer();
+            MutableText text = Text.empty();
+            boolean first = true;
+            if (data == null) {
+                data = ((MobCapTracker)player).getPlayerMobCapData();
+            }
+
+            if (data.isEmpty()) {
+                text.append(Text.literal("There are no mobs in this scope").formatted(Formatting.RED));
+                return text;
+            }
+
+            for (Map.Entry<SpawnGroup, int[]> entry : data.entrySet()) {
+                SpawnGroup group = entry.getKey();
+                if (group == SpawnGroup.MISC) {
+                    continue;
+                }
+                int current = entry.getValue()[0];
+                int total = entry.getValue()[1];
+                Formatting color;
+                float percent = (float) current / total;
+                if (percent >= 1.0) {
+                    color = Formatting.RED;
+                } else if (percent > 0.75) {
+                    color = Formatting.YELLOW;
+                } else {
+                    color = Formatting.GREEN;
+                }
+                if (!first) {
+                    text.append("\n");
+                } else {
+                    first = false;
+                }
+                text.append(Text.literal(group.asString() + ": ")).append(Text.literal(current + "/" + total).formatted(color));
+            }
+            return text;
+        } else {
+            context.getSource().sendError(Text.of("This command must be executed by a player"));
+            return null;
+        }
     }
 }
--- src/main/java/com/github/tatercertified/equicap/interfaces/MobCapTracker.java
+++ src/main/java/com/github/tatercertified/equicap/interfaces/MobCapTracker.java
@@ -1,6 +1,12 @@
 package com.github.tatercertified.equicap.interfaces;
 
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 public interface MobCapTracker {
     int getPlayerMobCap(SpawnGroup group);
@@ -11,4 +17,56 @@ public interface MobCapTracker {
     }
     int adjustedMobCapMaxSize(SpawnGroup group);
     void adjustMobCapBy(float percent);
+    default EnumMap<SpawnGroup, int[]> getPlayerMobCapData() {
+        EnumMap<SpawnGroup, int[]> count = new EnumMap<>(SpawnGroup.class);
+        for (SpawnGroup group : SpawnGroup.values()) {
+            int cap = this.getPlayerMobCap(group);
+            int total = this.adjustedMobCapMaxSize(group);
+            int[] existing = count.get(group);
+            if (existing == null) {
+                existing = new int[] {cap, total};
+            } else {
+                existing[0] += cap;
+                existing[1] += total;
+            }
+            count.put(group, existing);
+        }
+        return count;
+    }
+    static EnumMap<SpawnGroup, int[]> getDimensionMobCount(ServerWorld world) {
+        EnumMap<SpawnGroup, int[]> count = new EnumMap<>(SpawnGroup.class);
+        for (ServerPlayerEntity player : world.getPlayers()) {
+            for (SpawnGroup group : SpawnGroup.values()) {
+                int cap = ((MobCapTracker)player).getPlayerMobCap(group);
+                int total = ((MobCapTracker)player).adjustedMobCapMaxSize(group);
+                int[] existing = count.get(group);
+                if (existing == null) {
+                    existing = new int[] {cap, total};
+                } else {
+                    existing[0] += cap;
+                    existing[1] += total;
+                }
+                count.put(group, existing);
+            }
+        }
+        return count;
+    }
+    static EnumMap<SpawnGroup, int[]> getTotalMobCount(MinecraftServer server) {
+        EnumMap<SpawnGroup, int[]> count = new EnumMap<>(SpawnGroup.class);
+        for (ServerWorld world : server.getWorlds()) {
+            EnumMap<SpawnGroup, int[]> worldCount = getDimensionMobCount(world);
+
+            for (Map.Entry<SpawnGroup, int[]> entry : worldCount.entrySet()) {
+                int[] existing = count.get(entry.getKey());
+                if (existing == null) {
+                    existing = entry.getValue();
+                } else {
+                    existing[0] += entry.getValue()[0];
+                    existing[1] += entry.getValue()[1];
+                }
+                count.put(entry.getKey(), existing);
+            }
+        }
+        return count;
+    }
 }
